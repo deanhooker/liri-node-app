aefc9cf57a13c31a682ab060f9e578016a7dc827
Dean Hooker <deanhooker91@gmail.com>
2018-01-21 12:30:21 -0700

    twitter working

--- keys.js
+++ keys.js
@@ -1,10 +1,18 @@
 console.log('this is loaded');
 
-var twitterKeys = {
+const twitterKeys = {
   consumer_key: 'isDzhOS460u1vmJh7rgishptt',
   consumer_secret: 'cjBtgXXKC7INSCjboWlSrRqG5Yf8TPRBVtJXq4Uk4vD4KVXfKi',
   access_token_key: '955123964568743936-lspUfiBrP9yeWNdmFBYl6OqfnIsf9uP',
   access_token_secret: 'r0q0SWL0bHOyYRNoBFGWIcav5buFPJ4OgIK9CshyX7jmu',
 }
 
-module.exports = twitterKeys;
+// const spotifyKeys = {
+//     client_id = '1b0ed947b15e47cc9fcd0bf589e886c8',
+//     client_secret = 'c3bbf82f73374fea8076bcf6ccc93d21'
+// }
+
+module.exports = {
+    twitterKeys: twitterKeys
+    // spotifyKeys: spotifyKeys
+}
--- liri.js
+++ liri.js
@@ -1,22 +1,28 @@
+//set requirements
 const twitter = require("twitter");
 const spotify = require("node-spotify-api");
 const request = require("request");
+const fs = require("fs");
+const keys = require("./keys.js");
 
-// Make a JavaScript file named liri.js.
-// At the top of the liri.js file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
-// Make it so liri.js can take in one of the following commands:
+//set global variables
+const twitterKeys = new twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+});
+const spotifyKeys = keys.spotifyKeys;
 
-// my-tweets
-// spotify-this-song
-// movie-this
-// do-what-it-says
-
+//grab user input for which function to run
 let userCommand = process.argv[2];
 
+//run function depending on user input
 switch (userCommand) {
 
     case "my-tweets":
         myTweets();
+        console.log("Hi");
         break;
 
     case "spotify-this-song":
@@ -30,4 +36,22 @@ switch (userCommand) {
     case "do-what-it-says":
         doWhatItSays();
         break;
+
+    default:
+        console.log("ERROR: Valid commands are my-tweets, spotify-this-song, movie-this, do-what-it-says");
+}
+
+function myTweets() {
+
+    var params = { screen_name: 'deanhooker91', count: 20 };
+    twitterKeys.get("statuses/user_timeline", params, function (error, tweets, response) {
+        if (error) {
+            console.log("Error retrieving tweets");
+        }
+        else {
+            for (i = 0; i < tweets.length; i++) {
+                console.log("Tweet: " + tweets[i].text + " Date: " + tweets[i].created_at);
+            }
+        }
+    });
 }
